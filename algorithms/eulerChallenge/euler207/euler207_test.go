7b282b166a16b3bb3c179ab5925173e3d60ef107
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-04-14 20:48:10 +0200

    Prepare Euler for optimization

--- algorithms/eulerChallenge/euler207/euler207_test.go
+++ algorithms/eulerChallenge/euler207/euler207_test.go
@@ -1,15 +1,21 @@
 package euler207
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestLeastM_1(t *testing.T) {
-	fmt.Println("****** Starting test", "TestLeastM1", "******")
+// func TestLeastM_1(t *testing.T) {
+// 	fmt.Println("****** Starting test", "TestLeastM1", "******")
+//
+// 	checkLeastM(2, 3, 6, t)
+// 	checkLeastM(9, 20, 30, t)
+// 	checkLeastM(1, 10, 2550, t)
+// 	checkLeastM(1, 20, 14520, t)
+// }
 
-	checkLeastM(2, 3, 6, t)
-	checkLeastM(9, 20, 30, t)
+func TestLeastMultiM(t *testing.T) {
+	// _ = FindMulti([]int{2, 9, 1, 1}, []int{3, 20, 10, 20})
+	_ = FindMulti([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, []int{10, 10, 10, 10, 10, 10, 10, 10, 10, 10})
 }
 
 func checkLeastM(a, b, expect int, t *testing.T) {
