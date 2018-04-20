b656f0d26ba7747934a59e65ca210a1da6c5d6b4
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-04-20 22:38:55 +0200

    Quicksort with dutch partitioning

--- algorithms/quicksort/quicksort.go
+++ algorithms/quicksort/quicksort.go
@@ -1,5 +1,10 @@
 package quicksort
 
+import (
+	"math/rand"
+	"time"
+)
+
 type sorter func(int) bool
 
 func Sorted(ar []int) []int {
@@ -33,4 +38,49 @@ func filter(ar []int, fun sorter) []int {
 		}
 	}
 	return result
+}
+
+func DutchSort(ar []int, low, hi int) {
+	if low < hi {
+		pivotIndx := randInt(low, hi)
+		p, q := partitionDutchFlag(ar, low, hi, pivotIndx)
+		DutchSort(ar, low, p-1)
+		DutchSort(ar, q+1, hi)
+	}
+}
+
+func partitionDutchFlag(ar []int, low, hi, pivotIndx int) (int, int) {
+	var (
+		pivot   = ar[pivotIndx]
+		smaller = low
+		equal   = low
+		larger  = hi
+	)
+
+	for equal <= larger {
+		if ar[equal] < pivot {
+			swap(ar, smaller, equal)
+			smaller++
+			equal++
+
+		} else if ar[equal] == pivot {
+			equal++
+
+		} else {
+			swap(ar, equal, larger)
+			larger--
+		}
+	}
+	return smaller, larger
+}
+
+func swap(ar []int, aIndx, bIndx int) {
+	tmp := ar[aIndx]
+	ar[aIndx] = ar[bIndx]
+	ar[bIndx] = tmp
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
 }
--- algorithms/quicksort/quicksort_test.go
+++ algorithms/quicksort/quicksort_test.go
@@ -11,6 +11,9 @@ func TestSort(t *testing.T) {
 	testArray := []int{10, 0, 3, 9, 2, 14, 8, 27, 1, 5, 8, -1, 26}
 	mockSorted := []int{-1, 0, 1, 2, 3, 5, 8, 8, 9, 10, 14, 26, 27}
 	sorted := Sorted(testArray)
+	DutchSort(testArray, 0, len(testArray)-1)
+	fmt.Printf("testArray = %+v\n", testArray)
+
 	for i := 0; i < len(testArray); i++ {
 		if sorted[i] != mockSorted[i] {
 			t.Fatalf("Wrong sorting at index %d", i)
