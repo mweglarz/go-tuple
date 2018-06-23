ee1d968ed16717eb62753c0d7d69119c2ce1b1e2
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-06-23 17:34:16 +0200

    Cleaning

--- algorithms/hackerrank/bigger_is_greater/big/big.go
+++ algorithms/hackerrank/bigger_is_greater/big/big.go
@@ -3,38 +3,6 @@ package big
 import "strings"
 import "fmt"
 
-func minimize(runes []rune, pos int) {
-	swap := func(i, j int) {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-
-	for i := len(runes) - 1; i > pos; i-- {
-		for j := pos + 1; j < i; j++ {
-			if runes[j] > runes[i] {
-				swap(i, j)
-				minimize(runes, j)
-			}
-		}
-	}
-
-}
-
-func maximize(runes []rune) int {
-	swap := func(i, j int) {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-
-	for i := len(runes) - 2; i >= 0; i-- {
-		for j := i + 1; j < len(runes); j++ {
-			if runes[j] < runes[i] {
-				swap(i, j)
-				return j
-			}
-		}
-	}
-	return -1
-}
-
 func findMin(runes []rune, start int, validation func(rune) bool) int {
 	fmt.Println("findMin invoked")
 	fmt.Printf("runes = %+v\n", runes)
@@ -55,11 +23,6 @@ func findMin(runes []rune, start int, validation func(rune) bool) int {
 func BiggerIsGreater(word string) string {
 	fmt.Println("invoking bigger is greater, runes", []rune(word))
 	runes := []rune(strings.ToLower(word))
-	// pos := maximize(runes)
-	// if pos == -1 {
-	// 	return "no answer"
-	// }
-	// minimize(runes, pos)
 	swap := func(i, j int) {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
