81ab1e4c1eb8e0e1f76e2a8a89bca099e34e4c09
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-09-09 14:29:44 +0200

    Filtered obstacles

--- algorithms/hackerrank/queens-attack-2/queen/queen.go
+++ algorithms/hackerrank/queens-attack-2/queen/queen.go
@@ -8,45 +8,106 @@ type Pos struct {
 func MaxQueenDistance(queenPos Pos, obstacles []Pos) int {
 
 	var (
-		queenColMin Pos = queenPos
-		queenColMax Pos = queenPos
-		queenRowMin Pos = queenPos
-		queenRowMax Pos = queenPos
-		queenDiag1  Pos = queenPos
-		queenDiag2  Pos = queenPos
-		queenDiag3  Pos = queenPos
-		queenDiag4  Pos = queenPos
+		queenColMin Pos  = queenPos
+		queenColMax Pos  = queenPos
+		queenRowMin Pos  = queenPos
+		queenRowMax Pos  = queenPos
+		queenDiagSW *Pos = nil
+		queenDiagNW *Pos = nil
+		queenDiagNE *Pos = nil
+		queenDiagSE *Pos = nil
 	)
 
+	isDiagonal := func(pos Pos) bool {
+		return false
+	}
+	isSecDiagonal := func(pos Pos) bool {
+		return false
+	}
+
 	for _, pos := range obstacles {
-		_ = queenColMin
-		_ = queenColMax
-		_ = queenRowMin
-		_ = queenRowMax
-		_ = queenDiag1
-		_ = queenDiag2
-		_ = queenDiag3
-		_ = queenDiag4
 
 		// same col
 		if pos.x == queenPos.x {
 			if pos.y > queenColMax.y {
 				queenColMax = pos
+				continue
 			}
 			if pos.y < queenColMin.y {
 				queenColMin = pos
+				continue
 			}
 		}
 		// same row
 		if pos.y == queenPos.y {
 			if pos.x > queenRowMax.x {
 				queenRowMax = pos
+				continue
 			}
 			if pos.x < queenRowMin.x {
 				queenRowMin = pos
+				continue
+			}
+		}
+
+		if isDiagonal(pos) {
+			if pos.x < queenPos.x {
+				if queenDiagSW != nil {
+					if pos.x > queenDiagSW.x {
+						queenDiagSW = &pos
+					}
+					continue
+
+				} else {
+					queenDiagSW = &pos
+					continue
+				}
+
+			if pos.x > queenPos.x {
+				if queenDiagNE.x != nil {
+					if pos.x < queenDiagNE.x {
+						queenDiagNE = &pos
+					}
+					continue
+
+				} else {
+					queenDiagNE = &pos
+					continue
+				}
+			}
+		}
+
+		if isSecDiagonal(pos) {
+			if pos.x < queenPos.x {
+				if queenDiagNW != nil {
+					if pos.x > queenDiagNW.x {
+						queenDiagNW = &pos
+					}
+					continue
+
+				} else {
+					queenDiagNW = &pos
+					continue
+				}
+
+			if pos.x > queenPos.x {
+				if queenDiagSE.x != nil {
+					if pos.x < queenDiagSE.x {
+						queenDiagSE = &pos
+					}
+					continue
+
+				} else {
+					queenDiagSE = &pos
+					continue
+				}
 			}
 		}
 	}
 
+	return 0
+}
+
+func dist(pos1, pos2 Pos) int {
 	return 0
 }
