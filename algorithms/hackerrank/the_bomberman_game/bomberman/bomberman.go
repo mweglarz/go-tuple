656b557916f4e77c06a7a211aac8f8abc3127e08
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-09-10 21:39:59 +0200

    Update, added hashing func and initial transform

--- algorithms/hackerrank/the_bomberman_game/bomberman/bomberman.go
+++ algorithms/hackerrank/the_bomberman_game/bomberman/bomberman.go
@@ -1,6 +1,48 @@
 package bomberman
 
+import (
+	"crypto/md5"
+	"encoding/json"
+)
+
+type Pos struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+type Cell struct {
+	Pos      `json:"pos"`
+	Value    rune `json:"value"`
+	BombTime int8 `json:"bombTime"`
+}
+
+func (self Cell) Hash() (string, error) {
+	jsonBytes, err := json.Marshal(self)
+	if err != nil {
+		return "", err
+	}
+	bytes := md5.Sum(jsonBytes)
+	return string(bytes[:]), nil
+}
+
 func BomberMan(n int32, grid []string) []string {
 
 	return nil
+}
+
+func Transform(grid []string) [][]Cell {
+	var cells [][]Cell
+	var cellRow []Cell
+	for row, line := range grid {
+		cellRow = nil
+		for col, ch := range line {
+			pos := Pos{col, row}
+			cellRow = append(cellRow, Cell{pos, ch})
+		}
+		cells = append(cells, cellRow)
+	}
+	return cells
+}
+
+func GetRealmInitState([][]Cell) [][]Cell {
+
 }
--- algorithms/hackerrank/the_bomberman_game/bomberman/bomberman_test.go
+++ algorithms/hackerrank/the_bomberman_game/bomberman/bomberman_test.go
@@ -1,1 +1,28 @@
 package bomberman
+
+import (
+	"fmt"
+	"testing"
+)
+
+var testGrid []string = []string{
+	"..0",
+	"...",
+	"...",
+}
+
+func TestTransform(t *testing.T) {
+
+	result := Transform(testGrid)
+	fmt.Println(result)
+}
+
+func TestHashing(t *testing.T) {
+	c1 := Cell{Pos{9, 1}, 'd'}
+	c2 := Cell{Pos{9, 2}, 'd'}
+
+	h1, _ := c1.Hash()
+	h2, _ := c2.Hash()
+	fmt.Println(h1)
+	fmt.Println(h2)
+}
