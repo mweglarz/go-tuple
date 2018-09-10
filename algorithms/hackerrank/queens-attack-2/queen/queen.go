3d1a04292e71d15d3be061ee151e9b065195b3ce
Marcin Węglarz <marcin.weglarz01@gmail.com>
2018-09-10 18:45:52 +0200

    Update queens attack

--- algorithms/hackerrank/queens-attack-2/queen/queen.go
+++ algorithms/hackerrank/queens-attack-2/queen/queen.go
@@ -1,35 +1,129 @@
 package queen
 
+import (
+	"math"
+)
+
 type Pos struct {
-	x int
-	y int
+	x int32
+	y int32
 }
 
-func QueenMaxDistance(queenPos Pos, obstacles []Pos, size int) int {
-	optimizedObstacles := OptimizeObstacles(queenPos, obstacles, size)
-	_ = optimizedObstacles
-
-	return 0
+func (self Pos) Equal(pos Pos) bool {
+	return self.x == pos.x && self.y == pos.y
 }
 
-func OptimizeObstacles(queenPos Pos, obstacles []Pos, size int) []*Pos {
+func (self Pos) isEmpty() bool {
+	return self.x == 0 && self.y == 0
+}
+
+func (self Pos) Min() int32 {
+	min := self.x
+	if self.y < self.x {
+		min = self.y
+	}
+	return min
+}
+
+func (self Pos) Max() int32 {
+	max := self.x
+	if self.y > self.x {
+		max = self.y
+	}
+	return max
+}
+
+func QueenMaxDistance(queenPos Pos, obstacles [][]int32, size int32) int32 {
+	obs := OptimizeObstacles(queenPos, obstacles, size)
+
+	var dist int32 = 0
+
+	// check colMin
+	if queenPos.Equal(obs[0]) {
+		dist += queenPos.y - 1
+
+	} else {
+		dist += distance(queenPos, obs[0])
+	}
+
+	// check colMax
+	if queenPos.Equal(obs[1]) {
+		dist += size - queenPos.y
+
+	} else {
+		dist += distance(queenPos, obs[1])
+	}
+
+	// check rowMin
+	if queenPos.Equal(obs[2]) {
+		dist += queenPos.x - 1
+
+	} else {
+		dist += distance(queenPos, obs[2])
+	}
+
+	// check rowMax
+	if queenPos.Equal(obs[3]) {
+		dist += size - queenPos.x
+
+	} else {
+		dist += distance(queenPos, obs[3])
+	}
+
+	// check SW
+	if obs[4].isEmpty() {
+		dist += queenPos.Min() - 1
+
+	} else {
+		dist += distance(queenPos, obs[4])
+	}
+
+	// check NW
+	if obs[5].isEmpty() {
+		dist += Pos{queenPos.x, size + 1 - queenPos.y}.Min() - 1
+
+	} else {
+		dist += distance(queenPos, obs[5])
+	}
+
+	// check NE
+	if obs[6].isEmpty() {
+		dist += Pos{size + 1 - queenPos.x, size + 1 - queenPos.y}.Min() - 1
+
+	} else {
+		dist += distance(queenPos, obs[6])
+	}
+
+	// check SE
+	if obs[7].isEmpty() {
+		dist += Pos{size + 1 - queenPos.x, queenPos.y}.Min() - 1
+
+	} else {
+		dist += distance(queenPos, obs[7])
+	}
+
+	return dist
+}
+
+func OptimizeObstacles(queenPos Pos, obstacles [][]int32, size int32) []Pos {
 
 	var (
-		queenColMin Pos  = queenPos
-		queenColMax Pos  = queenPos
-		queenRowMin Pos  = queenPos
-		queenRowMax Pos  = queenPos
-		queenDiagSW *Pos = nil
-		queenDiagNW *Pos = nil
-		queenDiagNE *Pos = nil
-		queenDiagSE *Pos = nil
+		queenColMin Pos = queenPos
+		queenColMax Pos = queenPos
+		queenRowMin Pos = queenPos
+		queenRowMax Pos = queenPos
+		queenDiagSW Pos = Pos{}
+		queenDiagNW Pos = Pos{}
+		queenDiagNE Pos = Pos{}
+		queenDiagSE Pos = Pos{}
 	)
 
 	isDiagonal := isDiagonalGenerator(queenPos, size)
 	isSecDiagonal := isSecDiagonalGenerator(queenPos, size)
 
-	for _, pos := range obstacles {
+	for _, posArr := range obstacles {
 
+		pos := Pos{posArr[0], posArr[1]}
 		// same col
 		if pos.x == queenPos.x {
 			if pos.y > queenColMax.y {
@@ -55,27 +149,27 @@ func OptimizeObstacles(queenPos Pos, obstacles []Pos, size int) []*Pos {
 
 		if isDiagonal(pos) {
 			if pos.x < queenPos.x {
-				if queenDiagSW != nil {
+				if !queenDiagSW.isEmpty() {
 					if pos.x > queenDiagSW.x {
-						queenDiagSW = &pos
+						queenDiagSW = pos
 					}
 					continue
 
 				} else {
-					queenDiagSW = &pos
+					queenDiagSW = pos
 					continue
 				}
 			}
 
 			if pos.x > queenPos.x {
-				if queenDiagNE != nil {
+				if !queenDiagNE.isEmpty() {
 					if pos.x < queenDiagNE.x {
-						queenDiagNE = &pos
+						queenDiagNE = pos
 					}
 					continue
 
 				} else {
-					queenDiagNE = &pos
+					queenDiagNE = pos
 					continue
 				}
 			}
@@ -83,38 +177,38 @@ func OptimizeObstacles(queenPos Pos, obstacles []Pos, size int) []*Pos {
 
 		if isSecDiagonal(pos) {
 			if pos.x < queenPos.x {
-				if queenDiagNW != nil {
+				if !queenDiagNW.isEmpty() {
 					if pos.x > queenDiagNW.x {
-						queenDiagNW = &pos
+						queenDiagNW = pos
 					}
 					continue
 
 				} else {
-					queenDiagNW = &pos
+					queenDiagNW = pos
 					continue
 				}
 			}
 
 			if pos.x > queenPos.x {
-				if queenDiagSE != nil {
+				if !queenDiagSE.isEmpty() {
 					if pos.x < queenDiagSE.x {
-						queenDiagSE = &pos
+						queenDiagSE = pos
 					}
 					continue
 
 				} else {
-					queenDiagSE = &pos
+					queenDiagSE = pos
 					continue
 				}
 			}
 		}
 	}
 
-	return []*Pos{
-		&queenColMin,
-		&queenColMax,
-		&queenRowMin,
-		&queenRowMax,
+	return []Pos{
+		queenColMin,
+		queenColMax,
+		queenRowMin,
+		queenRowMax,
 		queenDiagSW,
 		queenDiagNW,
 		queenDiagNE,
@@ -122,20 +216,27 @@ func OptimizeObstacles(queenPos Pos, obstacles []Pos, size int) []*Pos {
 	}
 }
 
-func isDiagonalGenerator(queenPos Pos, size int) func(Pos) bool {
-	// generate linear func
+func isDiagonalGenerator(queenPos Pos, size int32) func(Pos) bool {
 	return func(pos Pos) bool {
-		return true
+		b := queenPos.y - queenPos.x
+		return pos.y == (pos.x + b)
 	}
 }
 
-func isSecDiagonalGenerator(queenPos Pos, size int) func(Pos) bool {
-	// generate linear func
+func isSecDiagonalGenerator(queenPos Pos, size int32) func(Pos) bool {
 	return func(pos Pos) bool {
-		return true
+		b := queenPos.y + queenPos.x
+		return pos.y == (-pos.x + b)
 	}
 }
 
-func distance(pos1, pos2 Pos) int {
-	return 0
+func distance(pos1, pos2 Pos) int32 {
+	if pos1.x == pos2.x {
+		abs := float64(pos1.y - pos2.y)
+		return int32(math.Abs(abs)) - 1
+
+	} else {
+		abs := float64(pos1.x - pos2.x)
+		return int32(math.Abs(abs)) - 1
+	}
 }
