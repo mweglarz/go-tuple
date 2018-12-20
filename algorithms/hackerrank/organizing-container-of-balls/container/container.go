44c2823fc6202029a66218fd3a8883f46f665710
Marcin Węglarz <marcin.weglarz01@gmail.com>
2018-12-20 22:24:57 +0100

    update

--- algorithms/hackerrank/organizing-container-of-balls/container/container.go
+++ algorithms/hackerrank/organizing-container-of-balls/container/container.go
@@ -4,20 +4,35 @@ func OrganizingContainers(container [][]int32) bool {
 
 	size := len(container)
 
-	// iterate types
+	// iterate containers
 	for i := 0; i < size; i++ {
-		// iterate containers
+		// iterate types
 		for j := 0; j < size; j++ {
-			if i == j {
+			if i == j || container[i][j] == 0 {
 				continue
 			}
 
-			min := min(container[i][j], container[j][i])
+			for l := j; l < size && container[i][j] != 0; l++ {
+				countSwapped := swap(container, i, j, l, i)
+				if l != j {
+					container[l][j] = container[l][j] + countSwapped
+				}
+			}
 
+			if container[i][j] != 0 {
+				return false
+			}
 		}
 	}
 
-	return false
+	return true
+}
+
+func swap(container [][]int32, i1, j1, i2, j2 int) int32 {
+	min := min(container[i1][j1], container[i2][j2])
+	container[i1][j1] = container[i1][j1] - min
+	container[i2][j2] = container[i2][j2] - min
+	return min
 }
 
 func min(a, b int32) int32 {
